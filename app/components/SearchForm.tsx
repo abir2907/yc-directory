5a96ff6e3dac89cac4666d9627d7666769a93b35
Abir Chodha <abirchodha1@gmail.com>
2025-02-11 02:49:07 +0530

    Update url on submission and get query from url

--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -3,9 +3,7 @@ import Form from "next/form";
 import SearchFormReset from "./SearchFormReset";
 import { Search } from "lucide-react";
 
-const SearchForm = () => {
-  const query = "Test";
-
+const SearchForm = ({ query }: { query?: string }) => {
   return (
     <Form action="/" scroll={false} className="search-form">
       <input
