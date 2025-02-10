bc80f436fc030b12f394a937f87d77be2a4bce33
Abir Chodha <abirchodha1@gmail.com>
2025-02-11 02:19:24 +0530

    Create Search Form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,5 @@
+import SearchForm from "../components/SearchForm";
+
 export default function Home() {
   return (
     <section className="pink_container">
@@ -9,6 +11,8 @@ export default function Home() {
       <p className="sub-heading !max-w-3xl">
         Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
       </p>
+
+      <SearchForm />
     </section>
   );
 }
