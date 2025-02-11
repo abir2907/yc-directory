c6422aca58e1813b570448a74562468ca8a787a0
Abir Chodha <abirchodha1@gmail.com>
2025-02-11 07:44:57 +0530

    Define author and startup schema

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,8 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
+
+import { startup } from "./startup";
+import { author } from "./author";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [author, startup],
+};
