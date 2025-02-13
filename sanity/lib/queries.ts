5f7f5aab7dd68f6717b5e1d7b198681a47c65936
Abir Chodha <abirchodha1@gmail.com>
2025-02-13 18:28:00 +0530

    Create query to get startup by ID

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -12,5 +12,21 @@ export const STARTUPS_QUERY =
   views,
   description,
   category,
-  image
+  image,
+}`);
+
+export const STARTUP_BY_ID_QUERY =
+  defineQuery(`*[_type == "startup" && _id == $id][0]{
+  _id, 
+  title, 
+  slug,
+  _createdAt,
+  author -> {
+    _id, name, username, image, bio
+  }, 
+  views,
+  description,
+  category,
+  image,
+  pitch,
 }`);
