2184bcf8e8a609d60ff3da77693ac36f7a29c30d
Abir Chodha <abirchodha1@gmail.com>
2025-02-16 15:59:33 +0530

    Add editor picks playlist

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,20 +1,30 @@
-import { formatDate } from "@/lib/utils";
 import { Suspense } from "react";
 import { client } from "@/sanity/lib/client";
-import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
-import Image from "next/image";
-import Link from "next/link";
+import {
+  PLAYLIST_BY_SLUG_QUERY,
+  STARTUP_BY_ID_QUERY,
+} from "@/sanity/lib/queries";
 import { notFound } from "next/navigation";
-import React from "react";
+import { formatDate } from "@/lib/utils";
+import Link from "next/link";
+import Image from "next/image";
+
 import markdownit from "markdown-it";
 import { Skeleton } from "@/components/ui/skeleton";
 import View from "@/components/View";
+import StartupCard, { StartupCardType } from "@/components/StartupCard";
 
 const md = markdownit();
 
 const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
-  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+
+  const [post, { select: editorPosts }] = await Promise.all([
+    client.fetch(STARTUP_BY_ID_QUERY, { id }),
+    client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+      slug: "editor-picks",
+    }),
+  ]);
 
   if (!post) return notFound();
 
@@ -51,7 +61,7 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
               <div>
                 <p className="text-20-medium">{post.author.name}</p>
-                <p className="text-16-medium !item-black-300">
+                <p className="text-16-medium !text-black-300">
                   @{post.author.username}
                 </p>
               </div>
@@ -73,12 +83,22 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
         <hr className="divider" />
 
-        {/* TODO: EDITOR SELECTED STARTUPS */}
-      </section>
+        {editorPosts?.length > 0 && (
+          <div className="max-w-4xl mx-auto">
+            <p className="text-30-semibold">Editor Picks</p>
 
-      <Suspense fallback={<Skeleton className="view_skeleton" />}>
-        <View id={id} />
-      </Suspense>
+            <ul className="mt-7 card_grid-sm">
+              {editorPosts.map((post: StartupCardType, i: number) => (
+                <StartupCard key={i} post={post} />
+              ))}
+            </ul>
+          </div>
+        )}
+
+        <Suspense fallback={<Skeleton className="view_skeleton" />}>
+          <View id={id} />
+        </Suspense>
+      </section>
     </>
   );
 };
--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -2,7 +2,8 @@ import { type SchemaTypeDefinition } from "sanity";
 
 import { startup } from "./startup";
 import { author } from "./author";
+import { playlist } from "@/sanity/schemaTypes/playlist";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [author, startup],
+  types: [author, startup, playlist],
 };
