f65a4eb897b3b548417b4b3ea417694ea76ae9f0
Abir Chodha <abirchodha1@gmail.com>
2025-02-16 15:21:46 +0530

    Create user page, make navbar mobile responsive

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,14 +1,14 @@
 import { defineQuery } from "next-sanity";
 
 export const STARTUPS_QUERY =
-  defineQuery(`*[_type == 'startup' && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc){
-  _id,
-  title,
+  defineQuery(`*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc) {
+  _id, 
+  title, 
   slug,
   _createdAt,
   author -> {
     _id, name, image, bio
-  },
+  }, 
   views,
   description,
   category,
@@ -32,19 +32,46 @@ export const STARTUP_BY_ID_QUERY =
 }`);
 
 export const STARTUP_VIEWS_QUERY = defineQuery(`
-  *[_type == "startup" && _id == $id][0]{
-      _id, views
-  }
+    *[_type == "startup" && _id == $id][0]{
+        _id, views
+    }
 `);
 
 export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`
-  *[_type == "author" && id == $id][0]{
-      _id,
-      id,
-      name,
-      username,
-      email,
-      image,
-      bio
-  }
-  `);
+*[_type == "author" && id == $id][0]{
+    _id,
+    id,
+    name,
+    username,
+    email,
+    image,
+    bio
+}
+`);
+
+export const AUTHOR_BY_ID_QUERY = defineQuery(`
+*[_type == "author" && _id == $id][0]{
+    _id,
+    id,
+    name,
+    username,
+    email,
+    image,
+    bio
+}
+`);
+
+export const STARTUPS_BY_AUTHOR_QUERY =
+  defineQuery(`*[_type == "startup" && author._ref == $id] | order(_createdAt desc) {
+  _id, 
+  title, 
+  slug,
+  _createdAt,
+  author -> {
+    _id, name, image, bio
+  }, 
+  views,
+  description,
+  category,
+  image,
+}`);
